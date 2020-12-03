c1b60ca09406705b0657fd37f9127a242e971f1e
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-12-03 11:01:28 -0300

    feat(modeu): creating the class and interfaces for the User entity

---
+++ src/model/User.ts
@@ -0,0 +1,39 @@
+export class User {
+   
+    constructor(
+        private id: string,
+        private name: string,
+        private email: string,
+        private nickname: string,
+        private password: string,
+    ){}
+
+    getId = () => this.id
+    getName = () => this.name
+    getEmail = () => this.email
+    getNickname = () => this.nickname
+    getPassword = () => this.password
+    
+    setId = (id: string) => this.id = id 
+    setName = (name: string) => this.name = name
+    setEmail = (email: string) => this.email = email
+    setNickname = (nickname: string) => this.nickname = nickname
+    setPassword = (password: string) => this.password = password
+
+    static toUserModel = (user: any):User =>{
+        return new User(user.id, user.name, user.email, user.nickname, user.password)
+    }
+
+}
+
+export interface UserInputDTO {
+    name: string
+    email: string
+    nickname: string
+    password: string 
+}
+
+export interface LoginInputDTO {
+    email: string
+    password: string
+}
