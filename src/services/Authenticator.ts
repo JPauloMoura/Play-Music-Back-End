f3e2cd24227f9acbad2984f9295676968b91c4c8
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-12-03 15:26:00 -0300

    refactor(Authenticator): generate access token from Id

--- src/services/Authenticator.ts
+++ src/services/Authenticator.ts
@@ -3,10 +3,8 @@ import * as jwt from "jsonwebtoken";
 export class Authenticator {
   public generateToken(input: AuthenticationData,
     expiresIn: string = process.env.ACCESS_TOKEN_EXPIRES_IN!): string {
-    const token = jwt.sign(
-      {
-        id: input.id,
-        role: input.role
+    const token = jwt.sign({
+      id: input.id
       },
       process.env.JWT_KEY as string,
       {
@@ -19,8 +17,7 @@ export class Authenticator {
   public getData(token: string): AuthenticationData {
     const payload = jwt.verify(token, process.env.JWT_KEY as string) as any;
     const result = {
-      id: payload.id,
-      role: payload.role
+      id: payload.id
     };
     return result;
   }
@@ -28,5 +25,4 @@ export class Authenticator {
 
 interface AuthenticationData {
   id: string;
-  role?: string;
 }
