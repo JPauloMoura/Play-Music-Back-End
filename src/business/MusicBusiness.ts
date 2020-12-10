9c9515b5e708a1440dfedc48d3a21fb16da4f93d
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-12-10 15:30:57 -0300

    feat(Business): creating the validations to add a new song

---
+++ src/business/MusicBusiness.ts
@@ -0,0 +1,54 @@
+import { InvalidInputError } from "../error/InvalidInputError";
+import { Authenticator } from "../services/Authenticator";
+import { IdGenerator } from "../services/IdGenerator";
+import { Music, MusicInputDTO } from "../model/Music";
+import { MusicDatabase } from "../data/MusicDatabase";
+import { GenreDatabase } from "../data/GenreDatabase";
+import { Genre } from "../model/Genre";
+import { GenreMusicDatabase } from "../data/GenreMusicDatabase";
+import { GenreMusic } from "../model/GenreMusic";
+
+export class MusicBusiness {
+    constructor(
+        private authenticator: Authenticator,
+        private musicDatabase: MusicDatabase,
+        private genreDatabase: GenreDatabase,
+        private genreMusicDatabase: GenreMusicDatabase,
+        private idGenerator: IdGenerator
+    ){}
+
+    public async addMusic(input: MusicInputDTO, token:string):Promise<void> {
+        try {
+            
+            if(!input.author || !input.album || !input.date || !input.file || !input.title || !input.genre.length){
+                throw new InvalidInputError("Invalid request body");
+            }
+
+            input.genre = input.genre[0].replace(' ','').split(',')
+
+            const user = this.authenticator.getData(token)
+            const idMusic = this.idGenerator.generate()
+            
+            const newMusic:Music = Music.toMusicModel({
+                ...input,
+                id: idMusic,
+                idUser: user.id
+            })
+
+            await this.musicDatabase.createMusic(newMusic) 
+
+            for(const index in input.genre){
+                const newGender: Genre = Genre.toGenreModel({id: this.idGenerator.generate(), name: input.genre[index]}) 
+                await this.genreDatabase.createGenre(newGender)
+
+                const dataGenre = await this.genreDatabase.getGenreByName(input.genre[index]) 
+
+                const newGenreMusic: GenreMusic = GenreMusic.toGenreMusicModel({idMusic: idMusic, idGenre: dataGenre.getId()})
+                await this.genreMusicDatabase.createGenreMusic(newGenreMusic)
+            }
+            
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+}
