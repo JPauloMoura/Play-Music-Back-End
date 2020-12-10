c5ab29a5104d7cf40ecf0345e532b5381f8e81f6
JPaulo-Moura <joaopaulo.034a@gmail.com>
2020-12-10 15:35:25 -0300

    feat(routes): configuring the routes for creating music

---
+++ src/routes/musicRoutes.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import { MusicController } from "../controller/MusicController"
+import multer from "multer"
+import config from "../middleware/config"
+import {authVerify} from "../middleware/authVerifey"
+
+export const musicRoute = express.Router()
+const musicController = new MusicController()
+const upload = multer(config)
+
+musicRoute.post("/upload", authVerify, upload.single("file"), musicController.createMusic)
